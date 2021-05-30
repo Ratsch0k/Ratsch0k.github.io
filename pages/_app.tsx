e3570e8bb47d37f87ca69638d740bf5e34e38386
Simon Kurz <simon.kurz@rocketmail.com>
2021-05-30 10:14:19 +0200

    Added two pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="h-full">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 export default MyApp
