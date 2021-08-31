909ab8f741dcd6f5ee479f0cb01af373ea3ff09d
Simon Kurz <simon.kurz@rocketmail.com>
2021-08-31 17:59:07 +0200

    Fixed type issues with translation jsons

--- i18n.ts
+++ i18n.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
-import de from './public/locales/de';
-import en from './public/locales/en';
+import de from './public/locales/de.json';
+import en from './public/locales/en.json';
 
 i18n
     .use(initReactI18next)
