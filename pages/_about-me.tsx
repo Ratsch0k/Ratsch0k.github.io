96bb57c71f07a05b9b88a322ba9077b8e74495a3
Simon Kurz <simon.kurz@rocketmail.com>
2021-09-08 22:36:53 +0200

    Added about me description to aboutme

--- pages/_about-me.tsx
+++ pages/_about-me.tsx
@@ -1,26 +1,54 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from 'react-i18next';
 
 function Block() {
   return (
     <div
-      className="bg-white inline-block h-full ml-2 mr-2 align-middle"
+      className='bg-white inline-block h-full ml-2 mr-2 align-middle'
       style={{
-        width: '20%',
+        width: '15%',
         height: '0.25rem'
       }}
     ></div>
   )
 }
 
+const LANGUAGES = [
+  'javascript',
+  'python',
+  'c++',
+  'java',
+  'rust',
+  'brainfuck',
+  'bash',
+]
+
 const AboutMe = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="h-full">
-      <div className="text-4xl w-full text-center md:text-6xl pt-4 absolute">
+    <div className='h-full overflow-hidden flex flex-col'>
+      <div className='text-4xl w-full text-center md:text-6xl pt-4'>
         <Block />
         <span>{t('aboutme.title')}</span>
         <Block />
+      </div>
+      <div className='h-full overflow-y-auto overflow-x-hidden'>
+        <div className='w-full flex flex-row items-stretch overflow-visible justify-between p-8'>
+          <div
+            className='w-full overflow-visible'
+          >
+            <Trans i18nKey='aboutme.myinfo' t={t}>
+              Hi...
+              <ul i18nIsDynamicList className='list-disc pl-8'>
+                {LANGUAGES.map((el) => <li>{t(`aboutme.langlist.${el}`)}</li>)}
+              </ul>
+              ...
+            </Trans>
+          </div>
+          <div className='hidden md:block md:flex-initial'>
+                <img alt={t('aboutme.picturealt')} className='hidden md:block h-full w-auto pl-4 object-cover' src='/images/personal_photo.jpg' />
+          </div>
+        </div>
       </div>
     </div>
   );
