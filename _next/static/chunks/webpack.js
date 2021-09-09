cc017be548197466ab083478b839b1ddbf8dd4c2
Simon Kurz <simon.kurz@rocketmail.com>
2021-09-09 23:29:30 +0200

    Updated to newest version  - Fixed breaking layout breaking changes

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28b804f48ec1c1eb3e52"; }
+/******/ 		__webpack_require__.h = function() { return "1426942602219d506e89"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
