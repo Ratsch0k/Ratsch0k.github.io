6298a0bb5684d7951d8bd10cc2c485c658d98d9a
Simon Kurz <simonlukas.98@gmail.com>
2022-01-31 23:51:31 +0100

    Added Dialog and DialogTitle

---
+++ components/Dialog.tsx
@@ -0,0 +1,37 @@
+import {
+  ComponentPropsWithoutRef
+} from 'react';
+import Modal, {ModalProps} from './Modal';
+
+export interface DialogProps extends ModalProps, ComponentPropsWithoutRef<'div'> {
+  elevation?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+/**
+ * ## Dialog
+ * Shown in front of the rest of page to get the users attention
+ * @param props Props
+ */
+const Dialog = (props: DialogProps) => {
+  const {open, children, className, ...rest} = props;
+
+  return (
+    <Modal open={open}>
+      <div
+        className='h-full w-full flex items-center'
+        style={{
+          backgroundColor: 'rgba(0.0, 0.0, 0.0, 0.3)'
+        }}
+      >
+        <div
+          className={`bg-white rounded-xl z-40 m-auto text-black p-4 ${className !== undefined ? className : ''}`}
+          {...rest}
+        >
+          {children}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default Dialog;
---
+++ components/DialogTitle.tsx
@@ -0,0 +1,39 @@
+import {
+  ComponentPropsWithoutRef
+} from 'react';
+import CloseIcon from './icons/CloseIcon';
+import IconButton from './IconButton';
+
+export interface DialogTitleProps extends ComponentPropsWithoutRef<'h2'> {
+  /**
+   * Callback for the close button
+   */
+  close?: () => void;
+
+  /**
+   * On default, on the right of the title a close button is shown.
+   * If this props is false the button wil not be shown.
+   */
+  noClose?: boolean;
+}
+
+const DialogTitle = (props: DialogTitleProps) => {
+  const {children, close, noClose, ...rest} = props;
+
+  return (
+    <h2 className='text-2xl font-bold align-baseline mb-4' {...rest}>
+      {children}
+      {
+        !noClose &&
+        <IconButton
+            onClick={close}
+            className='p-2 ml-2 !text-black hover:!bg-gray-200 float-right'
+        >
+          <CloseIcon />
+        </IconButton>
+      }
+    </h2>
+  )
+};
+
+export default DialogTitle;
