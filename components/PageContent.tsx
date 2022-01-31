18dbd5763467645054527950bbb61449e91ab811
Simon Kurz <simonlukas.98@gmail.com>
2022-02-01 00:01:13 +0100

    Added PageContent.tsx

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -1,16 +1,20 @@
 import {
-  ComponentPropsWithoutRef
-} from "../../../.local/share/JetBrains/Toolbox/apps/WebStorm/ch-0/213.5744.224/plugins/JavaScriptLanguage/jsLanguageServicesImpl/external/react";
+  ComponentPropsWithoutRef,
+  forwardRef,
+} from 'react';
 
-const PageContent = (props: ComponentPropsWithoutRef<'div'>) => {
+const PageContent = forwardRef<HTMLDivElement, ComponentPropsWithoutRef<'div'>>((props, ref) => {
   const {children, className, ...rest} = props;
 
   return (
-    <div className={'h-full overflow-auto max-w-4xl pt-20 md:pt-28 mx-auto scrollbar ' + className}
+    <div className={`h-full overflow-auto max-w-4xl pt-20 md:pt-28 mx-auto scrollbar ${className !== undefined ? className : ''}`}
+         ref={ref}
+         {...rest}
     >
       {children}
     </div>
   );
-}
+});
+PageContent.displayName = 'PageContent';
 
 export default PageContent;
