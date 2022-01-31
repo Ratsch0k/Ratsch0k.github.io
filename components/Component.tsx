4cc2ec404d126d3665f8b49f9938e42194f78cc0
Simon Kurz <simonlukas.98@gmail.com>
2022-01-31 23:58:55 +0100

    Removed Component.tsx

--- components/Component.tsx
+++
@@ -1,9 +0,0 @@
-import React, {CSSProperties} from 'react';
-
-export interface ComponentProps {
-  className?: string;
-  id?: string;
-  style?: CSSProperties;
-}
-
-export type Component<T = Record<string, unknown>> = React.FC<T & ComponentProps>;
