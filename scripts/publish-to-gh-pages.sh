0bb74eb673575a8955f9d9ebf7e78420d3f0d98e
Simon Kurz <32772268+Ratsch0k@users.noreply.github.com>
2022-07-18 22:36:13 +0200

    Develop (#7)
    Pushing on master will now deploy to Github pages

---
+++ scripts/publish-to-gh-pages.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "Install npm packages"
+yarn install
+
+echo "Build project"
+yarn build
+
+echo "Generate static html files"
+yarn export
+
+echo "Prepare for publishing"
+# This file is necessary to prevent github to any modification
+touch ./out/.nojekyll
+
+echo "Clone $URL"
+git clone "$URL" target
+cd target
+git checkout "$PAGES_BRANCH"
+
+# Remove everything
+rm -R ./*
+cd ..
+
+# Move html files into gh-pages branch
+mv out/ target/
+
+echo "Publish to gh-pages"
+cd target
+git push
